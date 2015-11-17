5e2837ad04566e8e79fb7bfb88b2a2a2e9e7bc6f
emodeen <emodeen@gmail.com>
2015-11-17 08:08:59 -0500

    Added main.js, only shown hamburger at mobile size

---
+++ main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+	$("#primary-nav .international").on("click",function(event){
+
+		event.preventDefault();
+
+		$("#primary-nav .international").toggleClass("active");
+
+		// If the nav item is active
+		if ($("#primary-nav .international").hasClass("active")) {
+			// If content is open
+			if($('#slide-down').hasClass('open')) { 
+
+				// .drop.open finds elements within #slide-down that have class drop and class open
+				// Hide all other sections, and remove class open for all other sections 
+				$('#slide-down .drop.open').hide().removeClass('open');
+
+				// Add open class to all elements having both class drop and class international
+				$('.drop.international').show().addClass('open');
+			} 
+
+			// If the content is not open
+			else { 
+
+				// Hide sections with class drop
+				$('#slide-down .drop').hide();
+
+				// Add class open to international section
+				$('.drop.international').show().addClass('open');
+
+				// Slide down content, and add class open to the section div
+				$('#slide-down').slideDown(150).addClass('open');			
+			}
+		}
+
+		// If the nav item is not active		
+		else {
+			$("#slide-down").slideUp().removeClass('open');
+			
+			$('#slide-down .drop.open').removeClass('open');
+		}
+	});
+});
